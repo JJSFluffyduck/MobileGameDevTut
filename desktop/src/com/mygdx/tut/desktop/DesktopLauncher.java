44511abe3d21e0a2190f9e14784f21922b4aa580
Jack Hall <jackhallsuper7@gmail.com>
2018-05-01 10:01:31 +0930

    JH-Basic project

---
+++ desktop/src/com/mygdx/tut/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.tut.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.tut.Week1;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Week1(), config);
+	}
+}
