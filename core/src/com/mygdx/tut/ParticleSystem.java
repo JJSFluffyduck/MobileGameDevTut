7c6aba761d5ec1aa78ff7ef350beac5853be3ade
Jack Hall <jackhallsuper7@gmail.com>
2018-05-28 23:38:54 +0930

    JH-Completed part 3

--- core/src/com/mygdx/tut/ParticleSystem.java
+++ core/src/com/mygdx/tut/ParticleSystem.java
@@ -1,5 +1,6 @@
 package com.mygdx.tut;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
@@ -23,6 +24,44 @@ public class ParticleSystem {
     Vector2[] velocity = new Vector2[MAX_PARTICLES];
 
     public void init(){
-        spritesheet = new Texture();
+        spritesheet = new Texture(Gdx.files.internal("explosion.png"));
+
+        explosionFrames[0] = new TextureRegion(spritesheet, 2, 2, 87, 87);
+        explosionFrames[1] = new TextureRegion(spritesheet, 94, 2, 87, 87);
+        explosionFrames[2] = new TextureRegion(spritesheet, 186, 2, 87, 87);
+        explosionFrames[3] = new TextureRegion(spritesheet, 278, 2, 87, 87);
+        explosionFrames[4] = new TextureRegion(spritesheet, 370, 2, 87, 87);
+        explosionFrames[5] = new TextureRegion(spritesheet, 2, 94, 87, 87);
+        explosionFrames[6] = new TextureRegion(spritesheet, 94, 94, 87, 87);
+        explosionFrames[7] = new TextureRegion(spritesheet, 186, 94, 87, 87);
+        explosionFrames[8] = new TextureRegion(spritesheet, 278, 94, 87, 87);
+        explosionFrames[9] = new TextureRegion(spritesheet, 370, 94, 87, 87);
+
+        for(int i =0; i<MAX_PARTICLES; i++){
+            type[i]=Type.NONE;
+            position[i]= new Vector2();
+            velocity[i]= new Vector2();
+        }
+    }
+
+    public int spawn(Type t){
+        if(t==null) return -1;
+
+        int i = -1;
+
+        for(int free = 0;free< MAX_PARTICLES; free++){
+            if(type[free] == Type.NONE){
+                i=free;
+                break;
+            }
+        }
+
+        if(i<0) return -1;
+
+        type[i] = t;
+        return i;
+    }
+    public void dispose () {
+        spritesheet.dispose();
     }
 }
